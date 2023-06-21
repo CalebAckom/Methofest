a924b3adaa4798745ba3ff41b89c3aeff70f729a
francis-buabin-owusu <francis.owusu@amalitech.com>
2023-06-21 23:01:25 +0000

    style: finished up responsiveness for all screen sizes

---
+++ src/components/landing-hero/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './styles.css'
+import logo from '../../assets/methodist.png'
+import mocker from '../../assets/3Dmocker.jpeg'
+
+const LandingHero = () => {
+
+    const handleScroll = (e) => {
+        const element = document.getElementById('scroll');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+
+    }
+    return (
+        <div className='landing-hero'>
+            <div className='upper-layer'>
+                <img src={logo} alt="" style={{ width: 80 }} />
+                <div>
+                    <h1 className='landing-title-text'>CONNEXIONAL METHOFEST 2023</h1>
+                    <h1 className='landing-directions-text'>DIRECTIONS TO BRANCH</h1>
+                    <h1 className='landing-footer-text'>THE METHODIST CHURCH GHANA</h1>
+                </div>
+            </div>
+            <div className='lower-layer'>
+                <div className="overlay">
+                    <div className='overlay-image'>
+                        <img src={mocker} alt="" />
+                    </div>
+                    <div className='overlay-footer'>
+                        <p>A Digital Directional Road Guide to Akyem Oda Methodist Cathedral</p>
+                        <button onClick={handleScroll}>View Dioceses</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingHero
--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './search-box.css'
+import searchIcon from '../../assets/search-icon.svg';
 
 export const SearchBox = ({ placeholder, handleChange }) => (
-    <input
-        className="search"
-        type='search'
-        placeholder={placeholder}
-        onChange={handleChange}
-    />
+    <div className="search-bar">
+        <img src={searchIcon} alt="" className="search-icon"/>
+        <input
+            className="search-input"
+            type='search'
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    </div>
 )
