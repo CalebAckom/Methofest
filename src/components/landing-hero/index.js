d2f45afc46d48927ceb0ebb3c211886aad7fa206
Daasebre <125558701+ObedBoateng@users.noreply.github.com>
2023-07-26 17:15:36 +0000

    Update index.js

--- src/components/landing-hero/index.js
+++ src/components/landing-hero/index.js
@@ -17,9 +17,9 @@ const LandingHero = () => {
             <div className='upper-layer'>
                 <img src={logo} alt="" style={{ width: 80 }} />
                 <div>
-                    <h1 className='landing-title-text'>CONNEXIONAL METHOFEST 2023</h1>
-                    <h1 className='landing-directions-text'>DIRECTIONS TO BRANCH</h1>
-                    <h1 className='landing-footer-text'>THE METHODIST CHURCH GHANA</h1>
+                    <h1 className='landing-title-text'>THE METHODIST CHURCH GHANA</h1>
+                    <h1 className='landing-directions-text'>CONNEXIONAL METHOFEST 2023</h1>
+                    <h1 className='landing-footer-text'>DIRECTIONS TO AYEM ODA</h1>
                 </div>
             </div>
             <div className='lower-layer'>
