46d7d431495ac2b9d2af74c543bcfdfa02c90563
CalebAckom <caleb.ackom@amalitech.com>
2023-07-27 12:15:05 +0000

    Adding Video Direction

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,9 +3,8 @@ import './card.css';
 
 export const Card = props => (
     <div className="card-container">
-        {/*<img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />*/}
-        <a target={"_blank"} href={`https://goo.gl/maps/${props.diocese.endpoint}`} className="button" rel="noreferrer">See Direction</a>
+        <a target={"_blank"} href={`https://goo.gl/maps/${props.diocese.endpoint}`} className="button" rel="noreferrer">Map Direction</a><br/>
+        <a target={"_blank"} href={`https://res.cloudinary.com/dovaohqno/video/upload/v1690459260/methofest/${props.diocese.video}`} className="button" rel="noreferrer">Video Direction</a>
         <h2> {props.diocese.name} </h2>
-        {/*<p> {props.monster.email} </p>*/}
     </div>
 );
--- src/helpers/dioceses-data.js
+++ src/helpers/dioceses-data.js
@@ -2,115 +2,115 @@ export const dioceses = [
     {
         name: "Cape Coast",
         id: "1",
-        email: "leanne.graham@mail.com",
+        video: "",
         endpoint: "REPpmHht5N46Qnuk7"
     },
     {
         name: "Accra",
         id: "2",
-        email: "ervin.howell@mail.com",
+        video: "dxgqn8j4azbcmvcsndn9.mp4",
         endpoint: "FmfUM3jAq3ASBKVd6"
     },
     {
         name: "Kumasi",
         id: "3",
-        email: "clementine.bauch@mail.com",
+        video: "",
         endpoint: "pdi4teyGWjbpB5br7"
     },
     {
         name: "Sekondi",
         id: "4",
-        email: "patricia.lebsack@mail.com",
+        video: "",
         endpoint: "u4R9iXkZuReBqmob9"
     },
     {
         name: "Winneba",
         id: "5",
-        email: "chelsey.dietrich@mail.com",
+        video: "",
         endpoint: "qfRSzMZ5P8nQCNaYA"
     },
     {
         name: "Koforidua",
         id: "6",
-        email: "dennis.schulist.mail.com",
+        video: "jvo6uoumqktnav86mhxn.mp4",
         endpoint: "mR9GYgobjy1rXuHA7"
     },
     {
         name: "Sunyani",
         id: "7",
-        email: "kurtis.weissnat@mail.com",
+        video: "",
         endpoint: "HXhAWtbtaeoty8RZ8"
     },
     {
         name: "Tarkwa",
         id: "8",
-        email: "nicholas.runolfsddottir@mail.com",
+        video: "",
         endpoint: "bjRge5panij4qBeB8"
     },
     {
         name: "Tamale",
         id: "9",
-        email: "glenna.reichert@mail.com",
+        video: "",
         endpoint: "SgRNExBzeUymmMLt7"
     },
     {
         name: "Effiduase",
         id: "10",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "73to2cTQwthj79nF7"
     },
     {
         name: "Obuasi",
         id: "11",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "nwKuebwTEfsEFPq98"
     },
     {
         name: "Tema",
         id: "12",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "rL3JKhRhAwwaEw2y6"
     },
     {
         name: "Nkawkaw",
         id: "13",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "Bidsq1V216oJGdB76"
     },
     {
         name: "Somanya",
         id: "14",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "WEJtDtcYB4zAfcMH8"
     },
     {
         name: "Foso",
         id: "15",
-        email: "clementina.dubuque@mail.com",
+        video: "jvo6uoumqktnav86mhxn.mp4",
         endpoint: "TcxdTCBJ614G7mwM9"
     },
     {
         name: "Goaso",
         id: "16",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "NCPEq3LtqUsPC7dy7"
     },
     {
         name: "Ho",
         id: "17",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "5dKY2WBm9L3buWLVA"
     },
     {
         name: "Sefwi Bekwai",
         id: "18",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "Y8men6HkmQFYDdyf6"
     },
     {
         name: "Wenchi",
         id: "19",
-        email: "clementina.dubuque@mail.com",
+        video: "",
         endpoint: "vPheiChQTLxn7jvA6"
     }
 ]
